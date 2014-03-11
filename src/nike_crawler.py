c8f36e80806f95d0e98efea6e6b6773e0c84530d
jbold569 <jason.bolden569@gmail.com>
2014-03-10 21:55:06 -0500

    Now uses regex to extract json objects from workout page

--- src/nike_crawler.py
+++ src/nike_crawler.py
@@ -1,26 +1,27 @@
 from base64 import b64encode
+from bs4 import BeautifulSoup as bs
 import datetime as dt
-import urllib2, json, gzip, time, argparse, os
+import urllib2, json, gzip, argparse, os, re
 
 auth_url = "https://api.twitter.com/oauth2/token"
 search_url = "https://api.twitter.com/1.1/search/tweets.json%s"
 base_query = "?q=nikeplus&count=100&include_entities=true&result_type=recent&since_id=%s"
 
-def serviceCall(url,data=None, headers={}, error_message="Request Failed"):
-	request = urllib2.Request(url,data,headers)
+def serviceCall(url, data=None, headers={}, error_message="Request Failed"):
+	request = urllib2.Request(url, data, headers)
 	response = None
 	try:
 		response = urllib2.urlopen(request)
 		return response.read()
 	except urllib2.HTTPError as e:
 		print error_message
-		print "Error %i: %s\nHTTP Method: %s"%(e.code,e.reason,request.get_method())
+		print "Error %i: %s\nHTTP Method: %s" % (e.code, e.reason, request.get_method())
 		return None
 	except ValueError:
 		print url
 
 def authenticate(consumer_key, consumer_secret):
-	#HTTP POST parameters
+	# HTTP POST parameters
 	headers = {
 		'Authorization': "Basic " + b64encode(consumer_key + ':' + consumer_secret),
 		'Content-Type': "application/x-www-form-urlencoded;charset=UTF-8"
@@ -29,11 +30,32 @@ def authenticate(consumer_key, consumer_secret):
 	dContent = json.loads(serviceCall(auth_url, data, headers, "Authentication failed"))
 	return dContent['access_token']
 
+def parseVars(html):
+	varPattern = re.compile('[\w._]+\s*=\s*{\s*["\'].*?\s*};', re.DOTALL)
+	dVariables = {}
+	
+	objects = re.findall(varPattern, html)
+	for obj in objects:
+		name, data = obj.split('=', 1)
+		# Since the variable are in dot notation grab the last var name
+		name = name.split('.')[-1].strip()
+		# remove the trailing ';' and convert the json string to dict or directly eval() it
+		try:
+			data = json.loads(data[:-1].encode("utf-8"))
+		except ValueError:
+			data = eval(data[:-1])
+		except:
+			data = None
+		
+		if data:
+			dVariables[name] = data 
+	return dVariables
+
 def main(args):
 	
 	max_id = "0"
 	try:
-		with  open("./.crawler_data/nikeplus/max_id",'r') as id_file:
+		with  open("./.crawler_data/nikeplus/max_id", 'r') as id_file:
 			max_id = id_file.read()
 	except IOError:
 		os.makedirs(".crawler_data/nikeplus/")
@@ -45,12 +67,12 @@ def main(args):
 	headers = {
 		'Authorization': "Bearer " + authenticate(args.consumer_key, args.consumer_secret) 
 	}
-	dContent = json.loads(serviceCall(url=search_url%(base_query%(max_id)), headers=headers, error_message="Search Failed"))
+	dContent = json.loads(serviceCall(url=search_url % (base_query % (max_id)), headers=headers, error_message="Search Failed"))
 
 
 	ct = dt.datetime.utcnow()
-	path = os.path.join(args.output,"%i/%i/%i/"%(ct.year,ct.month,ct.day))
-	filename = "%i-%02i-%02i_%02i-%02i.txt.gz"%(ct.year, ct.month, ct.day, ct.hour, ct.minute)
+	path = os.path.join(args.output, "%i/%i/%i/" % (ct.year, ct.month, ct.day))
+	filename = "%i-%02i-%02i_%02i-%02i.txt.gz" % (ct.year, ct.month, ct.day, ct.hour, ct.minute)
 
 	if not os.path.exists(path):
 		os.makedirs(os.path.join(path))
@@ -75,17 +97,18 @@ def main(args):
 				if len(urls) == 1:
 					nike_url = urls[0]['expanded_url']
 					nike_page = serviceCall(nike_url, error_message="Nike+ Page not Found")
+					
 					if nike_page:
-						tweet['nikeplus'] = {
-							'html': nike_page
-						}
+						nikeVars = parseVars(nike_page)
+						if not nikeVars: continue
+						tweet['nikeplus'] = nikeVars
 					try:
-						file.write(json.dumps(tweet, separators=(',',':'))+'\n')
+						file.write(json.dumps(tweet, separators=(',', ':')) + '\n')
 					except UnicodeDecodeError:
 						if args.verbose: print "Unicode characters in tweet, skipping..."
 						continue
 			try:
-				dContent = json.loads(serviceCall(url=search_url%(dContent['search_metadata']['next_results'])+"&since_id=%s"%(max_id), headers=headers, error_message="Search Failed"))	
+				dContent = json.loads(serviceCall(url=search_url % (dContent['search_metadata']['next_results']) + "&since_id=%s" % (max_id), headers=headers, error_message="Search Failed"))	
 			except KeyError:
 				break
 
@@ -96,4 +119,4 @@ if __name__ == "__main__":
 	parser.add_argument("-o", "--output", help="Crawler output directory", default="./")
 	parser.add_argument("-v", "--verbose", help="Enable debug information", action="store_true")
 
-	main(args = parser.parse_args())
+	main(args=parser.parse_args())
